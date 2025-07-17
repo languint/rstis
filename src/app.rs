b3bbfa2ba45594a9ed04c932fb6e59ed46e4471a
languint <longuint@proton.me>
2025-07-16 22:44:26 -0500

    feat: splashscreen loading animation

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,38 @@
+use gloo_timers::callback::Timeout;
 use yew::prelude::*;
 
-use crate::components::splashscreen::SplashScreen;
+use crate::components::splashscreen::{
+    SplashScreen, SPLASHSCREEN_LINE_COUNT, SPLASHSCREEN_LINE_DURATION,
+};
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let show_splash = use_state(|| true);
+
+    {
+        let show_splash = show_splash.clone();
+        use_effect_with((), move |_| {
+            let timeout = Timeout::new(
+                SPLASHSCREEN_LINE_DURATION * (SPLASHSCREEN_LINE_COUNT + 2),
+                move || {
+                    show_splash.set(false);
+                },
+            );
+            || {
+                timeout.cancel();
+            }
+        });
+    }
+
     html! {
         <div class="app">
-            <SplashScreen />
+            if *show_splash {
+                <SplashScreen />
+            } else {
+                <div class="main-content">
+                    <h1>{ "Welcome to the App!" }</h1>
+                </div>
+            }
         </div>
     }
 }
--- src/components/splashscreen.rs
+++ src/components/splashscreen.rs
@@ -1,16 +1,54 @@
-use yew::{function_component, html, Html};
+use gloo_timers::callback::Timeout;
+use yew::{function_component, html, use_effect_with, use_state, Html};
+
+pub const SPLASHSCREEN_LINE_DURATION: u32 = 300;
+pub const SPLASHSCREEN_LINE_COUNT: u32 = 6;
 
 #[function_component]
 pub fn SplashScreen() -> Html {
+    let active_lines = use_state(|| 0);
+    let is_done = use_state(|| false);
+
+    {
+        let active_lines = active_lines.clone();
+        let is_done = is_done.clone();
+        use_effect_with((), move |_| {
+            let mut timeouts = Vec::new();
+
+            for i in 1..=SPLASHSCREEN_LINE_COUNT {
+                let active_lines = active_lines.clone();
+                let timeout = Timeout::new((i * SPLASHSCREEN_LINE_DURATION) as u32, move || {
+                    active_lines.set(i);
+                });
+                timeouts.push(timeout);
+            }
+
+            let is_done = is_done.clone();
+            let done_timeout = Timeout::new(
+                ((SPLASHSCREEN_LINE_COUNT + 2) * SPLASHSCREEN_LINE_DURATION) as u32,
+                move || {
+                    is_done.set(true);
+                },
+            );
+            timeouts.push(done_timeout);
+
+            move || {
+                for timeout in timeouts {
+                    timeout.cancel();
+                }
+            }
+        });
+    }
+
     html! {
-        <div class="splashscreen">
+        <div class={if *is_done {"splashscreen hidden"} else {"splashscreen"}}>
             <div class="splashscreen-text">
-                <span class="splashscreen-inactive">{"          _   _     "}</span>
-                <span class="splashscreen-inactive">{"         | | (_)    "}</span>
-                <span class="splashscreen-inactive">{" _ __ ___| |_ _ ___ "}</span>
-                <span class="splashscreen-inactive">{"| '__/ __| __| / __|"}</span>
-                <span class="splashscreen-inactive">{"| |  \\__ \\ |_| \\__ \\"}</span>
-                <span class="splashscreen-inactive">{"|_|  |___/\\__|_|___/"}</span>
+                <span class={if *active_lines >= 1 {"splashscreen-active"} else {"splashscreen-inactive"}}>{"          _   _     "}</span>
+                <span class={if *active_lines >= 2 {"splashscreen-active"} else {"splashscreen-inactive"}}>{"         | | (_)    "}</span>
+                <span class={if *active_lines >= 3 {"splashscreen-active"} else {"splashscreen-inactive"}}>{" _ __ ___| |_ _ ___ "}</span>
+                <span class={if *active_lines >= 4 {"splashscreen-active"} else {"splashscreen-inactive"}}>{"| '__/ __| __| / __|"}</span>
+                <span class={if *active_lines >= 5 {"splashscreen-active"} else {"splashscreen-inactive"}}>{"| |  \\__ \\ |_| \\__ \\"}</span>
+                <span class={if *active_lines >= 6 {"splashscreen-active"} else {"splashscreen-inactive"}}>{"|_|  |___/\\__|_|___/"}</span>
             </div>
         </div>
     }
